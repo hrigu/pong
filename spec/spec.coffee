20037c8820964c9c6eb84f183cae0bf466276459
chrigu <chmuehlethaler@freesurf.ch>
2012-03-28 10:18:23 +0200

    mocked the context object

--- spec/spec.coffee
+++ spec/spec.coffee
@@ -1,12 +1,31 @@
-describe "a game", ->
+describe "game", ->
 	game = null
-	expectedTempo = 30
 	beforeEach ->
 		game = new pong.Game(20, 30)
-	it "has a tempo of #{expectedTempo}", ->
-		expect(game.tempo).toBe expectedTempo
-	it "has a ball", ->
-		expect(game.ball).toBeDefined
-	it "has two bats", ->
-		expect(game.bat_right).toBeDefined		
-		expect(game.bat_left).toBeDefined
+
+	describe "a new game", ->
+		expectedTempo = 30
+		it "has a tempo of #{expectedTempo}", ->
+			expect(game.tempo).toBe expectedTempo
+		it "has a ball", ->
+			expect(game.ball).toBeDefined
+		it "has two bats", ->
+			expect(game.bat_right).toBeDefined		
+			expect(game.bat_left).toBeDefined
+	describe "the run-loop", ->
+		class MockedContext
+			beginPath: -> "toBeSpied"
+			closePath: -> "toBeSpied"
+			clearRect: -> "mocked"
+			rect: (a, b, c, d) -> "mocked"
+			stroke: -> "mocked"		
+			fillRect: (a, b, c, d) -> "mocked"
+			fillText: (text, x, y) -> "mocked"
+			arc: (a, b, c, d, e, f) -> "mocked"		
+		it "works", ->
+			context = jasmine.createSpyObj('mockedContext', ['clearRect', 'beginPath', 'closePath', 'rect', 'stroke', 'fillRect', 'fillText', 'arc'])#new MockedContext()
+			#spyOn(context, 'clearRect')
+
+			game.run_loop(context)
+			expect(context.clearRect).toHaveBeenCalled();
+			expect(context.beginPath).toHaveBeenCalled();
--- spec/spec.js
+++ spec/spec.js
@@ -1,21 +1,73 @@
 (function() {
 
-  describe("a game", function() {
-    var expectedTempo, game;
+  describe("game", function() {
+    var game;
     game = null;
-    expectedTempo = 30;
     beforeEach(function() {
       return game = new pong.Game(20, 30);
     });
-    it("has a tempo of " + expectedTempo, function() {
-      return expect(game.tempo).toBe(expectedTempo);
+    describe("a new game", function() {
+      var expectedTempo;
+      expectedTempo = 30;
+      it("has a tempo of " + expectedTempo, function() {
+        return expect(game.tempo).toBe(expectedTempo);
+      });
+      it("has a ball", function() {
+        return expect(game.ball).toBeDefined;
+      });
+      return it("has two bats", function() {
+        expect(game.bat_right).toBeDefined;
+        return expect(game.bat_left).toBeDefined;
+      });
     });
-    it("has a ball", function() {
-      return expect(game.ball).toBeDefined;
-    });
-    return it("has two bats", function() {
-      expect(game.bat_right).toBeDefined;
-      return expect(game.bat_left).toBeDefined;
+    return describe("the run-loop", function() {
+      var MockedContext;
+      MockedContext = (function() {
+
+        function MockedContext() {}
+
+        MockedContext.prototype.beginPath = function() {
+          return "toBeSpied";
+        };
+
+        MockedContext.prototype.closePath = function() {
+          return "toBeSpied";
+        };
+
+        MockedContext.prototype.clearRect = function() {
+          return "mocked";
+        };
+
+        MockedContext.prototype.rect = function(a, b, c, d) {
+          return "mocked";
+        };
+
+        MockedContext.prototype.stroke = function() {
+          return "mocked";
+        };
+
+        MockedContext.prototype.fillRect = function(a, b, c, d) {
+          return "mocked";
+        };
+
+        MockedContext.prototype.fillText = function(text, x, y) {
+          return "mocked";
+        };
+
+        MockedContext.prototype.arc = function(a, b, c, d, e, f) {
+          return "mocked";
+        };
+
+        return MockedContext;
+
+      })();
+      return it("works", function() {
+        var context;
+        context = jasmine.createSpyObj('mockedContext', ['clearRect', 'beginPath', 'closePath', 'rect', 'stroke', 'fillRect', 'fillText', 'arc']);
+        game.run_loop(context);
+        expect(context.clearRect).toHaveBeenCalled();
+        return expect(context.beginPath).toHaveBeenCalled();
+      });
     });
   });
 
